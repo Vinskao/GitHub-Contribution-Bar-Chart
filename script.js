5e7a6d005067e8adeb7ca5cc39618275e7c118a8
Vinskao <tianyikao@gmail.com>
2023-09-27 12:03:42 +0800

    add npm

---
+++ script.js
@@ -0,0 +1,159 @@
+const dataPoints = [];
+const dateValues = [];
+const monthMap = {
+  '1月': 'Jan',
+  '2月': 'Feb',
+  '3月': 'Mar',
+  '4月': 'Apr',
+  '5月': 'May',
+  '6月': 'Jun',
+  '7月': 'Jul',
+  '8月': 'Aug',
+  '9月': 'Sep',
+  '10月': 'Oct',
+  '11月': 'Nov',
+  '12月': 'Dec',
+};
+addEventListener('load', () => {
+
+if (typeof window !== 'undefined') {
+	console.log('DOM已加載');
+	$(document).ready(function() {
+		let jsonData; 
+		$.getJSON("https://firebasestorage.googleapis.com/v0/b/graph-2cfc7.appspot.com/o/graph.json?alt=media&token=fae35fec-40f6-4244-a299-ff484d94bedc", function(response) {
+			jsonData = response; 
+			const contributionDays = jsonData.data.user.contributionsCollection.contributionCalendar.weeks
+				.flatMap(week => week.contributionDays);
+			let month;
+			let dayOfMonth;
+			let year;
+			let fullDate;
+			contributionDays.forEach(day => {
+				const date = new Date(day.date);
+				month = monthMap[date.toLocaleString('default', { month: 'short'})];
+				dayOfMonth = date.getDate(); 
+				year = date.getFullYear(); 
+				fullDate = `${year}-${month}-${dayOfMonth}`;
+				const contributionCount = day.contributionCount;
+				dataPoints.push([fullDate, contributionCount]);
+			});
+			
+			console.log(dataPoints);
+
+
+			let heightScale
+			let xScale
+			let xAxisScale
+			let yAxisScale
+
+			let width = 600
+			let height = 300
+			let padding = 40
+
+			let svg = d3.select('svg')
+
+			let drawCanvas = () => {
+				svg.attr('width', width)
+				svg.attr('height', height)
+			}
+
+			let generateScales = () => {
+				heightScale = d3.scaleLinear()
+								.domain([0 , d3.max(dataPoints, (item) => {
+									return item[1];
+								})])
+								.range([0 , height - (2 * padding)])
+
+				xScale = d3.scaleLinear()
+								.domain([0, dataPoints.length -1])
+								.range([padding, width - padding])
+
+				let datesArray = dataPoints.map((item) =>{
+					return new Date(item[0])
+				})
+
+				console.log(datesArray)
+
+				xAxisScale = d3.scaleTime()
+								.domain([d3.min(datesArray), d3.max(datesArray)])
+								.range([padding, width - padding])
+
+				yAxisScale = d3.scaleLinear()
+								.domain([0, d3.max(dataPoints , (item)=>{
+									return item[1]
+								})])
+								.range([height - padding, padding])
+
+			}
+
+			let drawBars = () => {
+				let tooltip = d3.select('body')
+									.append('div')
+									.attr('id','tooltips')
+									.style('visibility','hidden')
+									.style('width', 'auto')
+									.style('height', 'auto')
+				svg.selectAll('rect')
+					.data(dataPoints)
+					.enter()
+					.append('rect')
+					.attr('class', 'bar')
+					.attr('width', (width - (2 * padding))/ dataPoints.length)
+					.attr('data-date', (item) => {
+						return item[0]
+					})
+					.attr('data-count', (item) => {
+						return item[1]
+					})
+					.attr('height', (item) => {
+						return heightScale(item[1])
+					})
+					.attr('x', (item, index) => {
+						return xScale(index)
+					})
+					.attr('y', (item) => {
+						return (height - padding - heightScale(item[1]))
+					})
+					.on('mouseover', (item) => {
+						tooltip.transition()
+							.style('visibility', 'visible')
+
+						tooltip.text(item[0],item[1])
+					})
+					.on('mouseout', (item) => {
+						tooltip.transition()
+							.style('visibility', 'hidden')
+					})
+			}
+
+			let generateAxes = () => {
+				
+				let xAxis = d3.axisBottom(xAxisScale).tickFormat(d3.timeFormat('%Y-%m-%d'));
+				let yAxis = d3.axisLeft(yAxisScale);
+
+				svg
+					.append('g')
+					.call(xAxis)
+					.attr('id', 'x-axis')
+					.attr('transform', `translate(0, ${height - padding})`)
+
+				svg
+					.append('g')
+					.attr('id', 'y-axis')
+					.attr('transform', `translate(${padding}, 0)`)
+					.call(yAxis);
+			}
+
+			drawCanvas()
+			generateScales()
+			drawBars()
+			generateAxes()
+
+
+			
+		
+		});
+		
+	});
+};
+})
